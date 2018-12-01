ec18d003b44d43df1f759702f1c8e2b7b920664d
Thomas Finch <33233510+fincht96@users.noreply.github.com>
2018-12-01 11:47:55 +0000

    Update timer.cpp

--- src/timer.cpp
+++ src/timer.cpp
@@ -28,11 +28,11 @@ uint8_t Timer::getTimer()
 {
 	elapsedTime = m_clk.getElapsedTime().asSeconds(); 		// elapsed time in seconds since clock restarted
 
-	if (elapsedTime > (m_set_tim_value*0.016))						// time elapsed > time set, current timer value is 0
+	if (elapsedTime > (m_set_tim_value*0.016))			// time elapsed > time set, current timer value is 0
 		m_current_tim_value = 0;
 
 	else
-		m_current_tim_value = (uint8_t)(m_set_tim_value - (elapsedTime / 0.016));		// updates current timer value
+		m_current_tim_value = (uint8_t)(m_set_tim_value - (elapsedTime / 0.016));	// updates current timer value
 
 
 	return m_current_tim_value;
